1a135092080571e430c6cf80b096f8bf9a372c0d
Konstantin Mednikov <irondsd@gmail.com>
2019-12-12 16:34:43 +0300

    finalized navigator

--- navigation/NavContainer.js
+++ navigation/NavContainer.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react'
 import { createSwitchNavigator, createAppContainer } from 'react-navigation'
 import { Auth } from './Auth'
-// import { MainTabs } from './MainTabs'
+import { MainTabs } from './MainTabs'
 import SplashScreen from '../screens/SplashScreen'
-import HomeScreen from '../screens/HomeScreen'
 
 export default createAppContainer(
     createSwitchNavigator(
         {
             Loading: SplashScreen,
-            App: HomeScreen,
+            App: MainTabs,
             Auth: Auth,
         },
         {
