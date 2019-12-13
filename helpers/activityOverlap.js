282bc7b9b7ad3bf4b8ea05358d1476034e09ff4c
Konstantin Mednikov <irondsd@gmail.com>
2019-12-13 14:04:27 +0300

    overlaps working

--- helpers/activityOverlap.js
+++ helpers/activityOverlap.js
@@ -1,13 +1,17 @@
 import { overlaps } from '../properties'
 
 export function overlappingGreying(state, activityName) {
-    dateTime = (new Date().getTime() + '').substring(0, 10)
+    let dateTime = (new Date().getTime() + '').substring(0, 10)
 
     for (let i = 0; i < state.length; i++) {
-        if (state[i].time_ended == null || state[i].time_ended == 'null') continue
+        if (state[i].time_ended == null || state[i].time_ended == 'null')
+            continue
         if (!Object.keys(overlaps).includes(activityName)) continue
 
-        if (state[i].time_started < dateTime && state[i].time_ended > dateTime) {
+        if (
+            state[i].time_started < dateTime &&
+            state[i].time_ended > dateTime
+        ) {
             // console.log(overlaps[activityName].includes(state[i].activity_type))
             // console.log(state[i].activity_type)
             if (overlaps[activityName].includes(state[i].activity_type)) {
