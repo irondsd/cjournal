6cc01219cd98a3de7c8c4d029e1b115b656abecf
Konstantin Mednikov <irondsd@gmail.com>
2019-12-19 01:52:40 +0300

    permissions ios check

--- permissions/requestLocationPermissions.js
+++ permissions/requestLocationPermissions.js
@@ -1,6 +1,8 @@
-import {PermissionsAndroid} from 'react-native'
+import { PermissionsAndroid, Platform } from 'react-native'
 
 export default async function requestLocationPermissions() {
+    if (Platform.OS === 'ios') return true
+
     try {
         const granted = await PermissionsAndroid.request(
             PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
--- permissions/requestStoragePermission.js
+++ permissions/requestStoragePermission.js
@@ -1,44 +1,47 @@
-import { PermissionsAndroid } from 'react-native'
+import { PermissionsAndroid, Platform } from 'react-native'
 
 export default async function requestExternalWritePermission() {
-  try {
-    const granted = await PermissionsAndroid.request(
-      PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
-      {
-        title: 'Cardio Jounal External Storage Write Permission',
-        message: 'Cardio Jounal needs access to Storage data in your SD Card ',
-      },
-    )
-    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-      //If WRITE_EXTERNAL_STORAGE Permission is granted
-      //Calling the READ_EXTERNAL_STORAGE permission function
-      return await requestExternalReadPermission()
-    } else {
-      alert('WRITE_EXTERNAL_STORAGE permission denied')
+    if (Platform.OS === 'ios') return true
+
+    try {
+        const granted = await PermissionsAndroid.request(
+            PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+            {
+                title: 'Cardio Jounal External Storage Write Permission',
+                message:
+                    'Cardio Jounal needs access to Storage data in your SD Card ',
+            },
+        )
+        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+            //If WRITE_EXTERNAL_STORAGE Permission is granted
+            //Calling the READ_EXTERNAL_STORAGE permission function
+            return await requestExternalReadPermission()
+        } else {
+            alert('WRITE_EXTERNAL_STORAGE permission denied')
+        }
+    } catch (err) {
+        alert('Write permission err', err)
+        console.warn(err)
     }
-  } catch (err) {
-    alert('Write permission err', err)
-    console.warn(err)
-  }
 }
 async function requestExternalReadPermission() {
-  try {
-    const granted = await PermissionsAndroid.request(
-      PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
-      {
-        title: 'Cardio Jounal needs Storage Read Permission',
-        message: 'Cardio Jounal App needs access to your SD Card',
-      },
-    )
-    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-      return true
-    } else {
-      alert('READ_EXTERNAL_STORAGE permission denied')
-      return false
+    try {
+        const granted = await PermissionsAndroid.request(
+            PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
+            {
+                title: 'Cardio Jounal needs Storage Read Permission',
+                message: 'Cardio Jounal App needs access to your SD Card',
+            },
+        )
+        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+            return true
+        } else {
+            alert('READ_EXTERNAL_STORAGE permission denied')
+            return false
+        }
+    } catch (err) {
+        alert('Read permission err', err)
+        console.warn(err)
+        return false
     }
-  } catch (err) {
-    alert('Read permission err', err)
-    console.warn(err)
-    return false
-  }
 }
