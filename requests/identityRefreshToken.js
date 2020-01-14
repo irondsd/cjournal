a52173918be961b8c9894e28d5e8cfe40bed08f7
Konstantin Mednikov <irondsd@gmail.com>
2020-01-14 13:51:14 +0300

    refresh tokens done

---
+++ requests/identityRefreshToken.js
@@ -0,0 +1,17 @@
+import { identityTokenUrl } from '../properties'
+import { tokensReceived } from '../redux/actions'
+
+export default function identityRefreshToken(refresh_token) {
+    let formData = new FormData()
+    formData.append('grant_type', 'refresh_token')
+    formData.append('client_id', 'ApiClient')
+    formData.append('refresh_token', refresh_token)
+
+    return fetch(identityTokenUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'multipart/form-data',
+        },
+        body: formData,
+    }).then(res => res.json())
+}
