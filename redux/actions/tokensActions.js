5c20ebe38fde45e4293127728e119369c4bdbb80
Konstantin Mednikov <irondsd@gmail.com>
2020-01-14 13:50:45 +0300

    tokens redux done

--- redux/actions/tokensActions.js
+++ redux/actions/tokensActions.js
@@ -1,24 +1,6 @@
-export const tokensRefresh = tokens => {
-    // return {
-    //     type: 'TOKENS_REFRESH',
-    //     payload: tokens.refresh_token
-    // }
-    // return dispatch => {
-    //     fetch()
-    // .then(res => dispatch(tokensReceived(res)))
-    // .catch(err => dispatch(tokensRefreshError(error)))
-    // }
-}
-
 export const tokensReceived = tokens => {
     return {
         type: 'TOKENS_RECEIVED',
         payload: tokens,
-    }
-}
-
-export const tokensRefreshError = () => {
-    return {
-        type: 'TOKENS_REFRESH_ERROR',
     }
 }
--- redux/reducers/tokensReducer.js
+++ redux/reducers/tokensReducer.js
@@ -1,5 +1,6 @@
 import { tokensAsyncSave, removeAll } from '../../services/asyncStorage'
 import timestamp from '../../helpers/timestamp'
+import Tokens from '../../classes/Tokens'
 
 const initialState = {
     isLoggedIn: false,
@@ -12,20 +13,27 @@ export default function userReducer(state = initialState, { type, payload }) {
     switch (type) {
         case 'TOKENS_RECEIVED':
             let isLoggedIn = true
-            state = {
-                access_token: payload.access_token,
-                refresh_token: payload.refresh_token,
-                token_lifetime:
-                    payload.token_lifetime ||
-                    timestamp() + parseInt(payload.expires_in),
-                isLoggedIn: isLoggedIn,
-            }
+            // state = {
+            //     access_token: payload.access_token,
+            //     refresh_token: payload.refresh_token,
+            //     token_lifetime:
+            //         payload.token_lifetime ||
+            //         timestamp() + parseInt(payload.expires_in),
+            //     isLoggedIn: isLoggedIn,
+            // }
             // console.log(state)
+            let token_lifetime =
+                payload.token_lifetime ||
+                timestamp() + parseInt(payload.expires_in)
+            state = new Tokens(
+                payload.access_token,
+                payload.refresh_token,
+                token_lifetime,
+                isLoggedIn,
+            )
+
             tokensAsyncSave(state)
             return state
-        case 'TOKENS_REFRESH_ERROR':
-            removeAll()
-            return initialState
         default:
             return state
     }
