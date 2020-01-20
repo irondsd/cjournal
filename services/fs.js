e38d1f1ce47c33fae4f616f6b28747afbf802616
Konstantin Mednikov <irondsd@gmail.com>
2020-01-20 15:13:12 +0300

    error change

--- services/fs.js
+++ services/fs.js
@@ -14,7 +14,7 @@ export async function moveToParentDir(filepath, filename) {
 export async function downloadFile(url) {
     let filename = url.split('/')[1]
     let filepath = RNFS.DocumentDirectoryPath + '/' + filename
-    url = backendUrl + url
+    url = backendUrl + '/' + url
 
     // RNFS.readdir(RNFS.DocumentDirectoryPath).then(res => {
     //     console.log(res)
