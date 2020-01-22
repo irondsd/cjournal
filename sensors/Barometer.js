beab10878cd4274e06451aaeb9a52ab12d687c6b
Konstantin Mednikov <irondsd@gmail.com>
2020-01-22 17:10:52 +0300

    barometer init

--- sensors/Barometer.js
+++ sensors/Barometer.js
@@ -1,34 +1,58 @@
-import {barometer} from 'react-native-sensors'
+import { barometer } from 'react-native-sensors'
+import { average, altMeter } from '../helpers/math'
 
 export default class Barometer {
     constructor() {
         this.initialPressure = 0
         this.lastValue = 0
-        this.metersAcc = 0
-        this.metersDec = 0
         this.hPa = 0
         this.mmHg = 0
         this.pressures = []
         this.bar = null
     }
 
-    startUpdates() {
-        this.bar = barometer.subscribe(
-            ({pressure}) => {
-                if (initialPressure === 0 && pressures.length > 30) {
-                    initialPressure = average(pressures)
+    initialize(callback) {
+        bar = barometer.subscribe(
+            ({ pressure }) => {
+                if (this.initialPressure === 0 && this.pressures.length > 30) {
+                    this.initialPressure = average(this.pressures)
                 }
 
-                pressures.push(pressure)
-                if (pressures.length > 300) {
-                    pressures.splice(0, 1)
+                this.pressures.push(pressure)
+                this.lastValue = pressure
+                this.hPa = average(this.pressures)
+                this.mmHg = (
+                    average(this.pressures).toFixed(1) / 1.3332236
+                ).toFixed(0)
+
+                if (this.pressures.length > 300) {
+                    bar.unsubscribe()
+                }
+                callback()
+            },
+            error => {
+                console.log('Barometer is not available on this device')
+            },
+        )
+    }
+
+    startUpdates() {
+        this.bar = barometer.subscribe(
+            ({ pressure }) => {
+                if (this.initialPressure === 0 && this.pressures.length > 30) {
+                    this.initialPressure = average(this.pressures)
+                }
+
+                this.pressures.push(pressure)
+                if (this.pressures.length > 300) {
+                    this.pressures.splice(0, 1)
                 }
 
                 this.lastValue = pressure
-                this.hPa = average(pressures)
-                this.mmHg = (average(pressures).toFixed(1) / 1.3332236).toFixed(
-                    0,
-                )
+                this.hPa = average(this.pressures)
+                this.mmHg = (
+                    average(this.pressures).toFixed(1) / 1.3332236
+                ).toFixed(0)
             },
             error => {
                 console.log('Barometer is not available on this device')
@@ -42,7 +66,7 @@ export default class Barometer {
     static calibrate(seconds = 60) {
         // console.log('Calibrating barometer...')
         bar = barometer.subscribe(
-            ({pressure}) => {},
+            ({ pressure }) => {},
             error => {
                 // console.log('Barometer is not available on this device')
             },
