8712a5a359c4cdacba0339d0ad0084e8e26de409
Konstantin Mednikov <irondsd@gmail.com>
2020-01-31 13:31:05 +0300

    idinv update

--- services/idinvWatcher.js
+++ services/idinvWatcher.js
@@ -6,13 +6,13 @@ export async function idinvWatcher(id, access_token, idinv) {
     // console.log(RNFS.DocumentDirectoryPath)
     let filepath = '/storage/emulated/0/.incart/state.ini'
     RNFS.exists(filepath).then(res => {
-        // console.log('state.ini', res)
         RNFS.readFile(filepath, 'utf8').then(res => {
             let is_recording
             let parsed_idinv
+            // console.log('idinv', res)
             try {
                 is_recording = res.match(/is_recording = (\w+)/)[1]
-                parsed_idinv = res.match(/idinv = (\w+).dat/)[1]
+                parsed_idinv = res.match(/idinv = (\w+)/)[1]
                 if (idinv !== parsed_idinv) {
                     // post request to update
                     userUpdateIdinv(id, access_token, parsed_idinv).then(
