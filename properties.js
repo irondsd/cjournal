38e8b7768ce50b4a63904421dfaf22145f584239
Konstantin Mednikov <irondsd@gmail.com>
2020-02-12 15:28:46 +0300

    new identity

--- properties.js
+++ properties.js
@@ -1,7 +1,7 @@
 // urls
 export const backendUrl = 'http://217.197.236.243:3333'
 export const apiUrl = `${backendUrl}/api/`
-export const identityUrl = `http://217.197.236.242:7050`
+export const identityUrl = `http://identity.incart.ru:7050`
 export const registrationUrl = `${identityUrl}/Identity/Account/Register`
 export const profileEditUrl = `${identityUrl}/Identity/Account/Manage`
 export const identityTokenUrl = `${identityUrl}/connect/token`
