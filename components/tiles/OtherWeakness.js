7a2c0f9a350fcec78ce9aab07d991791cb5b888f
Konstantin Mednikov <irondsd@gmail.com>
2020-02-17 13:56:38 +0300

    other tiles

---
+++ components/tiles/OtherWeakness.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import ActivityItem from '../ActivityItem'
+import { strings } from '../../localizations'
+import { activityInstantSave } from '../../helpers/activityInstantSave'
+import { activity_types, paths } from '../../properties'
+import { iconPicker } from '../../helpers/iconPicker'
+
+const name = activity_types.OtherWeakness
+let clicked = false
+
+export default class Tile extends Component {
+    render() {
+        return (
+            <ActivityItem
+                text={strings[name]}
+                img={iconPicker(name)}
+                navigation={this.props.navigation}
+                color="#ada"
+                imgScale={0.8}
+                onPress={() => {
+                    this.props.navigation.navigate(paths.Other, {
+                        sender: name,
+                    })
+                }}
+                onLongPress={() => {
+                    this.props.navigation.navigate(paths.Other, {
+                        sender: name,
+                    })
+                }}
+            />
+        )
+    }
+}
