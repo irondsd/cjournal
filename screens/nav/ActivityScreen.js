ddcec9cd7dade6a36f61b22ba4e6d15c04a8d1c6
Konstantin Mednikov <irondsd@gmail.com>
2020-02-18 14:23:41 +0300

    activity screen

--- screens/nav/ActivityScreen.js
+++ screens/nav/ActivityScreen.js
@@ -8,6 +8,9 @@ import SexTile from '../../components/tiles/SexTile'
 import ToiletTile from '../../components/tiles/ToiletTile'
 import ShowerTile from '../../components/tiles/ShowerTile'
 import OtherActivity from '../../components/tiles/OtherActivity'
+import AlcoholTile from '../../components/tiles/AlcoholTile'
+import MealTile from '../../components/tiles/MealTile'
+import SmokingTile from '../../components/tiles/SmokingTile'
 
 class ActivityScreen extends Component<Props> {
     static navigationOptions = {
@@ -17,6 +20,9 @@ class ActivityScreen extends Component<Props> {
     render() {
         return (
             <TileWrapper>
+                <MealTile navigation={this.props.navigation} />
+                <AlcoholTile navigation={this.props.navigation} />
+                <SmokingTile navigation={this.props.navigation} />
                 <SexTile navigation={this.props.navigation} />
                 <ToiletTile navigation={this.props.navigation} />
                 <ShowerTile navigation={this.props.navigation} />
