6bf64ad580f0f3690a4232d50b5f53b40e3040e4
Konstantin Mednikov <irondsd@gmail.com>
2020-02-25 16:53:28 +0300

    todos

--- services/fs.js
+++ services/fs.js
@@ -19,7 +19,7 @@ export async function downloadFile(url) {
     // RNFS.readdir(RNFS.DocumentDirectoryPath).then(res => {
     //     console.log(res)
     // })
-
+    // TODO: check it works
     if (await RNFS.exists(filepath)) {
         // console.log('EXISTS')
     } else {
