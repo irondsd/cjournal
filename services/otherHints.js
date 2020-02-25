afb130fe5d1dcff9566a742c5c090180df927204
Konstantin Mednikov <irondsd@gmail.com>
2020-02-25 16:21:08 +0300

    hints working

---
+++ services/otherHints.js
@@ -0,0 +1,25 @@
+import AsyncStorage from '@react-native-community/async-storage'
+
+export async function addHint(name, item) {
+    if (!item) return
+
+    let list = await AsyncStorage.getItem(name)
+    list = JSON.parse(list)
+
+    if (!list.includes(item)) {
+        list = [item, ...list]
+    }
+
+    if (list.length > 10) list.pop()
+
+    console.log(list)
+    AsyncStorage.setItem(name, JSON.stringify(list))
+}
+
+export async function loadHints(name) {
+    return new Promise((resolve, reject) => {
+        AsyncStorage.getItem(name).then(res => {
+            resolve(JSON.parse(res))
+        })
+    })
+}
