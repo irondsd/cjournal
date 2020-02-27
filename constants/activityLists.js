7250c6cfdf5e90d0cce62fbfc206711570f0897b
Konstantin Mednikov <irondsd@gmail.com>
2020-02-27 13:44:56 +0300

    activity lists

---
+++ constants/activityLists.js
@@ -0,0 +1,16 @@
+import { activityTypes } from './activityTypes'
+
+export const pillsList = [
+    activityTypes.MedicineTest,
+    activityTypes.ReliefOfAttack,
+    activityTypes.CourseTherapy,
+]
+
+export const othersList = [
+    activityTypes.OtherActivity,
+    activityTypes.OtherComplaints,
+    activityTypes.OtherLoad,
+    activityTypes.OtherPain,
+    activityTypes.OtherWeakness,
+    activityTypes.OtherEmotions,
+]
