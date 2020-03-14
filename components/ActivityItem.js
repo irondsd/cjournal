56ddaf6777e028e6f24d6857f42a75782fe5b19c
Konstantin Mednikov <irondsd@gmail.com>
2020-03-14 10:56:10 +0300

    new activity item

--- components/ActivityItem.js
+++ components/ActivityItem.js
@@ -1,83 +1,23 @@
 import React, { Component } from 'react'
+import { Text, View, StyleSheet, Image, TouchableOpacity } from 'react-native'
 import {
-    Platform,
-    View,
-    Text,
-    StyleSheet,
-    Image,
-    TouchableOpacity,
-    Dimensions,
-} from 'react-native'
+    tileMargin,
+    imgSize,
+    borderRadius,
+    tileSize,
+} from '../constants/styles'
 import { pSBC } from '../helpers/colors'
 import LinearGradient from 'react-native-linear-gradient'
 import { showToast, showError } from '../services/toast'
 import { strings } from '../localizations'
 
-export default class ActivityItem extends Component {
+export default class aTile extends Component {
+    static defaultProps = { shadeColor: '#000' }
+
     render() {
-        let marginTop =
-            (Dimensions.get('window').height -
-                70 -
-                64 -
-                10 -
-                20 -
-                tileSize / 2 -
-                tileSize * 4) /
-            5
-        let text = this.props.text || 'Undefined'
-        if (Dimensions.get('window').width / 20 < marginTop)
-            marginTop = Dimensions.get('window').width / 20
-
-        let shadeColor = '#000000dd'
-        if (this.props.shadeColor) shadeColor = this.props.shadeColor
-        let numberOfLines = 1
-        if (text.length > 14) {
-            numberOfLines = 2
-        }
-        let color = this.props.color
-        let imgSize = tileSize * 0.7 * this.props.imgScale
-
-        if (numberOfLines > 1) {
-            imgSize = imgSize * 0.8
-        }
-
-        let margin =
-            styles.buttonActivity.width / 2 -
-            styles.buttonActivity.borderWidth -
-            parseInt(imgSize / 2)
-        if (this.props.disabled) color = pSBC(0.7, color)
-        let propsStyles = {
-            text: {
-                height: tileSize / (numberOfLines === 1 ? 5 : 4),
-                fontSize:
-                    Platform.OS === 'android'
-                        ? text.length > 14
-                            ? 9
-                            : 14
-                        : null,
-            },
-            img: {
-                height: imgSize ? imgSize : 70,
-                width: imgSize ? imgSize : 70,
-                top: margin ? margin : 14,
-                left: margin ? margin : 14,
-            },
-            buttonActivity: {
-                backgroundColor: this.props.color,
-                marginTop: marginTop,
-                marginBottom: 0,
-            },
-        }
-        let mainColor = this.props.color
-        shadeColor = pSBC(-0.5, this.props.color, shadeColor)
-
-        if (this.props.disabled) {
-            mainColor = '#aaa'
-            shadeColor = '#aaa'
-        }
-
         return (
             <TouchableOpacity
+                style={styles.container}
                 onLongPress={() => {
                     if (!this.props.disabled) {
                         this.props.onLongPress()
@@ -93,53 +33,44 @@ export default class ActivityItem extends Component {
                     }
                 }}>
                 <LinearGradient
-                    style={[styles.buttonActivity, propsStyles.buttonActivity]}
-                    colors={[mainColor, shadeColor]}
+                    style={[styles.box]}
+                    colors={[this.props.color, this.props.shadeColor]}
                     start={{ x: 0.0, y: 0.0 }}
                     end={{ x: 1.0, y: 1.0 }}>
-                    <Image
-                        style={[styles.img, propsStyles.img]}
-                        source={this.props.img}
-                    />
-                    <Text
-                        adjustsFontSizeToFit
-                        numberOfLines={numberOfLines}
-                        style={[styles.text, propsStyles.text]}>
-                        {this.props.text}
-                    </Text>
+                    <Image style={styles.img} source={this.props.img} />
                 </LinearGradient>
+                <View style={styles.titleBox}>
+                    <Text style={styles.text}>{this.props.text}</Text>
+                </View>
             </TouchableOpacity>
         )
     }
 }
 
-const tileSize = Dimensions.get('window').width / 3.75
-const borderRadius = tileSize / 6
-const margin = Dimensions.get('window').width / 46.875
-let imgSize = tileSize * 0.7
-const imgMargin = (tileSize - imgSize) / 2
-
 const styles = StyleSheet.create({
-    buttonActivity: {
-        width: tileSize,
-        margin: margin,
+    container: {
+        flex: 1,
         height: tileSize,
-        justifyContent: 'flex-end',
-        borderRadius: borderRadius,
-        borderWidth: 2,
-        borderColor: '#00000015',
-        borderStyle: 'solid',
-        paddingLeft: 5,
-        paddingRight: 5,
+        marginLeft: tileMargin,
+        marginRight: tileMargin,
     },
-    text: {
-        textAlign: 'center',
-        color: '#fff',
-        fontSize: 14,
+    box: {
+        height: tileSize * 0.9,
+        backgroundColor: '#666',
+        borderRadius: borderRadius,
+        alignItems: 'center',
+        justifyContent: 'center',
     },
     img: {
-        height: imgSize,
         width: imgSize,
-        position: 'absolute',
+        height: imgSize,
+    },
+    titleBox: {
+        height: tileMargin * 4.5,
+    },
+    text: {
+        color: 'black',
+        fontSize: 14,
+        textAlign: 'center',
     },
 })
