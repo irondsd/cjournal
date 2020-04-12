99c84d4d343e3d7b2fcb9a2cd55e33e3a211574a
Konstantin Mednikov <irondsd@gmail.com>
2020-04-12 10:16:18 +0300

    app-auth setup

--- ios/cjournal/AppDelegate.h
+++ ios/cjournal/AppDelegate.h
@@ -7,9 +7,9 @@
 
 #import <React/RCTBridgeDelegate.h>
 #import <UIKit/UIKit.h>
-
-@interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate>
-
+#import "RNAppAuthAuthorizationFlowManager.h"
+@interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate, RNAppAuthAuthorizationFlowManager>
+@property (nonatomic, weak)id<RNAppAuthAuthorizationFlowManagerDelegate>authorizationFlowManagerDelegate;
 @property (nonatomic, strong) UIWindow *window;
 
 @end
