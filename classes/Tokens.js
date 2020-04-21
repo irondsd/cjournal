95729b943ee4dfe3e204732c6452af882c0febd3
Konstantin Mednikov <irondsd@gmail.com>
2020-04-21 19:12:05 +0300

    identity eliminated in favor of tokens functions

--- classes/Tokens.js
+++ classes/Tokens.js
@@ -1,8 +1,13 @@
 import timestamp from '../helpers/timestamp'
 import store from '../redux/store'
 import identityRefreshToken from '../requests/identityRefreshToken'
-import { tokensReceived } from '../redux/actions'
+import { tokensReceived, updateUser } from '../redux/actions'
 import { updateTokenBeforeExpiration } from '../constants'
+import { authorize, refresh } from 'react-native-app-auth'
+import { identityServerConfig } from '../constants/config'
+import { loginConfirm } from '../requests/loginConfirm'
+import { Alert } from 'react-native'
+import { strings } from '../localizations'
 
 export default class Tokens {
     constructor(access_token, refresh_token, token_lifetime, isLoggedIn) {
@@ -10,21 +15,60 @@ export default class Tokens {
         this.refresh_token = refresh_token
         this.token_lifetime = token_lifetime
         this.isLoggedIn = isLoggedIn
+        this.ongoingUpdate = false
     }
 
-    refresh() {
+    static async receive() {
         return new Promise((resolve, reject) => {
-            identityRefreshToken(this.refresh_token)
-                .then(res => {
-                    if (!res.ok) throw new Error(res.status)
-                    if (res.access_token && res.refresh_token) {
-                        store.dispatch(tokensReceived(res))
-                        resolve(res)
-                    } else {
-                        throw new Error('No tokens received')
+            authorize(identityServerConfig)
+                .then(result => {
+                    if (result.accessToken) {
+                        result = tokensConverter(result)
+                        loginConfirm(result.access_token)
+                            .then(res => {
+                                if (res.error) {
+                                    Alert.alert(
+                                        strings.Error,
+                                        strings.CantConnectBackend,
+                                    )
+                                    throw new Error(res)
+                                }
+                                store.dispatch(updateUser(res))
+                                store.dispatch(tokensReceived(result))
+
+                                resolve(result)
+                            })
+                            .catch(err => {
+                                reject(err)
+                            })
                     }
                 })
                 .catch(err => {
+                    reject(err)
+                })
+        })
+    }
+
+    async update() {
+        return new Promise((resolve, reject) => {
+            if (this.ongoingUpdate) return reject('already updating')
+            this.ongoingUpdate = true
+
+            refresh(identityServerConfig, {
+                refreshToken: this.refresh_token,
+            })
+                .then(result => {
+                    this.ongoingUpdate = false
+                    if (result.accessToken) {
+                        result = tokensConverter(result)
+
+                        store.dispatch(tokensReceived(result))
+                        resolve(result)
+                    }
+                })
+                .catch(err => {
+                    this.ongoingUpdate = false
+                    console.log('error', err.message)
                     reject(err)
                 })
         })
@@ -37,5 +81,13 @@ export default class Tokens {
     expiresSoon() {
         // 10 minutes before it expires
         return this.token_lifetime - timestamp() < updateTokenBeforeExpiration
+    }
+}
+
+function tokensConverter(tokens) {
+    return {
+        access_token: tokens.accessToken,
+        refresh_token: tokens.refreshToken,
+        token_lifetime: timestamp(new Date(tokens.accessTokenExpirationDate)),
     }
 }
