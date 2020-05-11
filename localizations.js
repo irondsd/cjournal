ca4a72940b9e3831a7f3980634f48f4f50bdb129
Konstantin Mednikov <irondsd@gmail.com>
2020-05-11 21:22:59 +0300

    folder rename

--- localizations.js
+++ localizations.js
@@ -1,5 +1,5 @@
 import LocalizedStrings from 'react-native-localization'
-import { en, es, ru } from './localizations/index'
+import { en, es, ru } from './localization/index'
 
 export const strings = new LocalizedStrings({
     en: en,
