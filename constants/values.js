84ef686de472219b37eaea71cace798f1306dc9a
Konstantin Mednikov <irondsd@gmail.com>
2020-05-21 21:35:33 +0300

    value of state ini

--- constants/values.js
+++ constants/values.js
@@ -3,3 +3,4 @@ export const listUpdateInterval = 3000 // ms
 export const updateTokenBeforeExpiration = 600 // seconds
 export const walkingDuration = 360 // seconds = 6 minutes
 export const locationRetryLimit = 5 // for instant activity with location
+export const stateIniFilePath = 'storage/emulated/0/.incart/state.ini'
