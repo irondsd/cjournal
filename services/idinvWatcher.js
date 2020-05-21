476699b55d9c4eec08493795aca58a0b6f0314b1
Konstantin Mednikov <irondsd@gmail.com>
2020-05-21 21:33:46 +0300

    idinv watcher update

--- services/idinvWatcher.js
+++ services/idinvWatcher.js
@@ -1,13 +1,13 @@
 import userUpdateIdinv from '../requests/userUpdateIdinv'
-
+import { stateIniFilePath } from '../constants'
+import { userFetchData } from '../requests/userFetchData'
 var RNFS = require('react-native-fs')
 
 export async function idinvWatcher(id, access_token, idinv) {
-    // console.log(RNFS.DocumentDirectoryPath)
-    let filepath = '/storage/emulated/0/.incart/state.ini'
+    let filepath = stateIniFilePath
+    console.log('reading', filepath)
     RNFS.exists(filepath).then(res => {
         if (!res) return // no state.ini file
-
         RNFS.readFile(filepath, 'utf8').then(res => {
             let is_recording
             let parsed_idinv
@@ -19,7 +19,10 @@ export async function idinvWatcher(id, access_token, idinv) {
                     // post request to update
                     userUpdateIdinv(id, access_token, parsed_idinv).then(
                         res => {
-                            // if (res.ok) console.log('idinv updated')
+                            if (res.ok) {
+                                userFetchData(id, access_token)
+                                console.log('idinv updated')
+                            }
                         },
                     )
                 } else {
