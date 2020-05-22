10661d7203f5a1b87e20962f233d328ad6039332
Konstantin Mednikov <irondsd@gmail.com>
2020-05-22 15:49:32 +0300

    idinv watcher request permission

--- services/idinvWatcher.js
+++ services/idinvWatcher.js
@@ -1,11 +1,15 @@
 import userUpdateIdinv from '../requests/userUpdateIdinv'
 import { stateIniFilePath } from '../constants'
 import { userFetchData } from '../requests/userFetchData'
+import { requestExternalReadPermission } from '../permissions/requestStoragePermission'
 var RNFS = require('react-native-fs')
 
 export async function idinvWatcher(id, access_token, idinv) {
     let filepath = stateIniFilePath
-    console.log('reading', filepath)
+    // console.log('reading', filepath)
+
+    await requestExternalReadPermission()
+
     RNFS.exists(filepath).then(res => {
         if (!res) return // no state.ini file
         RNFS.readFile(filepath, 'utf8').then(res => {
