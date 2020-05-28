2554afa371660582bf3c831a5d082b1bac783830
Konstantin Mednikov <irondsd@gmail.com>
2020-05-28 12:57:01 +0300

    map component

--- components/Map.js
+++ components/Map.js
@@ -1,21 +1,47 @@
 import React, { Component } from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import { Text, View, StyleSheet, Image } from 'react-native'
 // import MapView from 'react-native-maps'
 import { borderGrey } from '../constants'
 
-export default class maps extends Component {
+export default class Map extends Component {
+    state = {
+        latitude: '',
+        longitude: '',
+    }
+
+    componentDidMount() {
+        this.setCoords()
+    }
+
+    componentDidUpdate() {
+        if (!this.state.latitude) {
+            this.setCoords()
+        }
+    }
+
+    setCoords = () => {
+        if (this.props.coords[0]) {
+            this.setState({
+                latitude: this.props.coords[0].latitude,
+                longitude: this.props.coords[0].longitude,
+            })
+        }
+    }
+
     render() {
         return (
             <View style={styles.container}>
-                <Text style={styles.text}>{`${this.props.coords}`}</Text>
-                {/* <MapView
-                    initialRegion={{
-                        latitude: 37.78825,
-                        longitude: -122.4324,
-                        latitudeDelta: 0.0922,
-                        longitudeDelta: 0.0421,
+                <Image
+                    style={styles.tinyLogo}
+                    ImageResizeMode={'center'}
+                    source={{
+                        uri:
+                            'https://geospatialmedia.s3.amazonaws.com/wp-content/uploads/2019/07/Apple-maps-app.png',
                     }}
-                /> */}
+                />
+                <Text style={styles.text}>{`No map available\nlatitude: ${
+                    this.state.latitude
+                }\nlongitude: ${this.state.longitude}`}</Text>
             </View>
         )
     }
@@ -26,8 +52,8 @@ const styles = StyleSheet.create({
         borderRadius: 5,
         borderWidth: 0.5,
         borderColor: borderGrey,
-        paddingLeft: 15,
-        paddingRight: 15,
+        // paddingLeft: 15,
+        // paddingRight: 15,
         height: 250,
         width: '100%',
         backgroundColor: 'white',
@@ -37,7 +63,14 @@ const styles = StyleSheet.create({
         marginBottom: 5,
     },
     text: {
-        fontSize: 17,
+        textAlign: 'center',
+        fontSize: 25,
         top: 13,
+        color: 'black',
+    },
+    tinyLogo: {
+        position: 'absolute',
+        width: '100%',
+        height: 250,
     },
 })
