3192bb14fcd1a9ba426ec79867560b043cbc7728
Konstantin Mednikov <irondsd@gmail.com>
2020-06-03 16:36:49 +0300

    qr base64 validity checks

--- helpers/encode.js
+++ helpers/encode.js
@@ -9,8 +9,19 @@ export function decode(value) {
     let test = value.split(' ')
 
     if (test[0] === qrEncodePrefix) {
-        return base64.decode(test[1])
+        let base64String = test[1]
+
+        if (!base64String) return
+        if (!isValidBase64(base64String)) return
+
+        return base64.decode(base64String)
     } else {
         return
     }
+}
+
+function isValidBase64(string) {
+    let base64regex = /^([0-9a-zA-Z+/]{4})*(([0-9a-zA-Z+/]{2}==)|([0-9a-zA-Z+/]{3}=))?$/
+    console.log(base64regex.test(string))
+    return base64regex.test(string)
 }
