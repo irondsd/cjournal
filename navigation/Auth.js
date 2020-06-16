973cb141207a5e942bb955eb792424bbbcaaf79d
Konstantin Mednikov <irondsd@gmail.com>
2020-06-16 18:15:46 +0300

    add new registr screen

--- navigation/Auth.js
+++ navigation/Auth.js
@@ -4,11 +4,18 @@ import { createAppContainer } from 'react-navigation'
 import LoginScreen from '../screens/LoginScreen'
 import { createStackNavigator } from 'react-navigation-stack'
 import WelcomeScreen from '../screens/WelcomeScreen'
+import RegisterScreen from '../screens/RegisterScreen'
 
 export const Auth = createStackNavigator(
     {
         Welcome: {
             screen: WelcomeScreen,
+            navigationOptions: ({ navigation }) => ({
+                header: null,
+            }),
+        },
+        Register: {
+            screen: RegisterScreen,
             navigationOptions: ({ navigation }) => ({
                 header: null,
             }),
