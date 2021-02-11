6304e194254457898369587720bad46e6a94871c
Konstantin Mednikov <irondsd@gmail.com>
2021-02-11 10:54:47 +0300

    new redux compose

--- redux/store.js
+++ redux/store.js
@@ -2,16 +2,10 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import { allReducers } from './reducers/allReducers'
 import thunk from 'redux-thunk'
 
-const store = createStore(
+const middleware = [thunk]
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+export default createStore(
     allReducers,
-
-    compose(
-        applyMiddleware(thunk),
-        window.navigator.userAgent
-            ? window.__REDUX_DEVTOOLS_EXTENSION__ &&
-                  window.__REDUX_DEVTOOLS_EXTENSION__()
-            : compose,
-    ),
+    composeEnhancers(applyMiddleware(...middleware)),
 )
-
-export default store
