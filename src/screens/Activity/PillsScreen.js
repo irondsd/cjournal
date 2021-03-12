328fa6536de33d191cd5ff4c37006dd89eeb5222
Konstantin Mednikov <irondsd@gmail.com>
2021-03-12 11:13:51 +0300

    new pills screen

--- src/screens/Activity/PillsScreen.js
+++ src/screens/Activity/PillsScreen.js
@@ -1,183 +1,123 @@
-import React, { Component } from 'react'
-import { StyleSheet, View, TouchableOpacity, Picker } from 'react-native'
-import DateTimePicker from 'react-native-modal-datetime-picker'
-import { displayDate, displayTime, getUtcOffset } from '../../helpers/dateTime'
-import { strings } from '../../localization'
-import { addActivity } from '../../redux/actions'
-import { connect } from 'react-redux'
-import TimePicker from '../../components/TimePicker'
+import React, { useState, useEffect } from 'react'
+import { StyleSheet, View } from 'react-native'
 import {
     backgroundColor,
     durations,
     paths,
     activityTypes,
     defaultStyles,
+    prescriptions,
 } from '../../constants'
-import DurationPicker from '../../components/DurationPicker'
-import Activity from '../../classes/Activity'
-import { findTaskById, findLatestTask } from '../../classes/Task'
-import TakePhoto from '../../components/TakePhoto'
-import DropDownInput from '../../components/DropDownInput'
-import SaveButton from '../../components/SaveButton'
+import {
+    NavigationStackScreenComponent,
+    NavigationStackScreenProps,
+} from 'react-navigation-stack'
+import { strings } from '../../localization'
 import timestamp from '../../helpers/timestamp'
+import { connect } from 'react-redux'
+import { addActivity } from '../../redux/actions'
+import { findLatestTask } from '../../classes/Task'
+import DateTimePicker from 'react-native-modal-datetime-picker'
+import { displayDate, displayTime, getUtcOffset } from '../../helpers/dateTime'
+import { TimePicker } from '../../components/TimePicker.tsx'
+import TakePhoto from '../../components/TakePhoto'
+import DropDownInput from '../../components/DropDownInput.tsx'
+import SaveButton from '../../components/SaveButton'
+import Activity from '../../classes/Activity'
 
-class PillsScreen extends Component {
-    constructor(props) {
-        super(props)
+export const PillsScreen = ({ navigation, user, tasks, add }) => {
+    const params = navigation?.state?.params
+    const [activity, setActivity] = useState({})
+    const [data, setData] = useState({})
+    const [pillsList, setPillsList] = useState([])
 
-        this.state = {
-            photoFile: '',
-            dateTime: new Date(),
-            pill: '',
-            pills: [],
-            input: false,
-            activity_type: null,
-            task: null,
-        }
-
-        this.changeDateTime = this.changeDateTime.bind(this)
-        this.onPillChange = this.onPillChange.bind(this)
-        this.clearPhoto = this.clearPhoto.bind(this)
-    }
-
-    static navigationOptions = ({ navigation }) => ({
-        title: `${strings[navigation.state.params.sender]}`,
-    })
-
-    componentDidMount() {
-        let dateTime = new Date()
-        dateTime.setSeconds(0)
-        dateTime.setMilliseconds(0)
-        let activity_type = this.props.navigation.state.params.sender
-        this.setState({
-            dateTime: dateTime,
-            activity_type: activity_type,
+    const updateActivityValue = (key, value) => {
+        setActivity({
+            ...activity,
+            [key]: value,
         })
-
-        let pills
-        switch (activity_type) {
-            case activityTypes.CourseTherapy:
-                pills = this.props.user.course_therapy
-                break
-            case activityTypes.ReliefOfAttack:
-                pills = this.props.user.relief_of_attack
-                break
-            case activityTypes.MedicineTest:
-                pills = this.props.user.tests
-                break
-            default:
-                pills = ['Error']
-                break
-        }
-        this.setState({ pills: pills })
-
-        let task_id = findLatestTask(this.props.tasks, activity_type)
-        let pill = null
-        let task = null
-
-        if (this.props.navigation.state.params?.task) {
-            task_id = this.props.navigation.state.params.task
-        }
-
-        task = findTaskById(this.props.tasks, task_id)
-        if (task) {
-            pill = task.data.pill
-        }
-
-        this.setState({
-            pill: pill,
-            task: task_id,
+    }
+    const updateDataValue = (key, value) => {
+        console.log(key, value)
+        setData({
+            ...data,
+            [key]: value,
         })
     }
 
-    changeDateTime(dateTime) {
-        this.setState({
-            dateTime: dateTime,
-        })
+    const clearPhoto = () => {
+        //
     }
 
-    handleSubmit() {
-        let activity = Activity.init(
-            this.state.activity_type,
-            timestamp(this.state.dateTime),
+    const submit = () => {
+        const newAct = new Activity.init(
+            activity.activity_type,
+            activity.time_started,
             undefined,
-            this.state.task_id || undefined,
-            '',
-            { pill: this.state.pill },
+            activity.task,
+            undefined,
+            data,
         )
-        if (this.state.photoFile) activity.data.photoFile = this.state.photoFile
-        this.props.add(activity)
-        this.props.navigation.navigate(paths.Home)
+        add(newAct)
+        navigation.navigate(paths.Home)
     }
 
-    static getDerivedStateFromProps(nextProps, prevState) {
-        if (nextProps.navigation.state.params) {
-            if (nextProps.navigation.state.params.image) {
-                return {
-                    photoFile: nextProps.navigation.state.params.image.uri,
+    useEffect(() => {
+        // setup activity
+        const time_started = timestamp()
+        const activity_type = params?.sender
+        const task = params?.task || findLatestTask(tasks, activity_type)
+
+        setActivity({
+            activity_type,
+            time_started,
+            task,
+        })
+
+        // set pills list
+        const pillsType = prescriptions[activity_type]
+        const patient = user.patient || {}
+        const pillsList = patient[pillsType] || []
+        setPillsList(pillsList)
+
+        // set up photo
+        const photoFile = params.image?.uri
+        setData({ photoFile: photoFile })
+    }, [params])
+
+    return (
+        <View style={defaultStyles.container}>
+            <TimePicker
+                time={activity.time_started}
+                onChange={value => updateActivityValue('time_started', value)}
+            />
+            <DropDownInput
+                placeholder={strings.Drug}
+                options={pillsList}
+                onChange={value => updateDataValue('pill', value)}
+                open={true}
+                value={data.pill}
+            />
+            <TakePhoto
+                photo={data.photoFile}
+                openCamera={() =>
+                    navigation.navigate(paths.Camera, {
+                        returnTo: paths.Pills,
+                    })
                 }
-            }
-            return {}
-        } else {
-            return { photoFile: '' }
-        }
-    }
+                removePhoto={clearPhoto}
+            />
+            <SaveButton
+                style={styles.button}
+                title={strings.Save}
+                onPress={submit}
+            />
+        </View>
+    )
+}
 
-    onPillChange(itemValue) {
-        this.setState({
-            pill: itemValue,
-        })
-    }
-
-    clearPhoto() {
-        this.setState({
-            photoFile: null,
-        })
-        this.props.navigation.setParams({ image: null })
-    }
-
-    render() {
-        if (!this.state.activity_type) return null
-        return (
-            <View style={defaultStyles.container}>
-                <TimePicker
-                    dateTime={this.state.dateTime}
-                    handler={this.changeDateTime}
-                />
-                <View
-                    style={
-                        Platform.OS === 'ios'
-                            ? { zIndex: 10, width: '100%' }
-                            : { width: '100%' }
-                    }>
-                    <DropDownInput
-                        placeholder={strings.Drug}
-                        list={this.state.pills}
-                        onChangeText={this.onPillChange}
-                        open={true}
-                        value={this.state.pill}
-                    />
-                </View>
-                <TakePhoto
-                    photo={this.state.photoFile}
-                    openCamera={() =>
-                        this.props.navigation.navigate(paths.Camera, {
-                            returnTo: paths.Pills,
-                        })
-                    }
-                    removePhoto={this.clearPhoto}
-                />
-                <SaveButton
-                    style={styles.button}
-                    title={strings.Save}
-                    onPress={() => {
-                        this.handleSubmit.bind(this)
-                        this.handleSubmit()
-                    }}
-                />
-            </View>
-        )
-    }
+PillsScreen.navigationOptions = {
+    title: 'title figure out',
 }
 
 function mapStateToProps(state) {
@@ -192,8 +132,6 @@ const mapDispatchToProps = dispatch => ({
         dispatch(addActivity(activity))
     },
 })
-
-export default connect(mapStateToProps, mapDispatchToProps)(PillsScreen)
 
 const styles = StyleSheet.create({
     center: {
@@ -210,3 +148,5 @@ const styles = StyleSheet.create({
         justifyContent: 'flex-end',
     },
 })
+
+export default connect(mapStateToProps, mapDispatchToProps)(PillsScreen)
