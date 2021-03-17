fcd57483a895dcc65215db8a3c2c5259af31d43c
Konstantin Mednikov <irondsd@gmail.com>
2021-03-17 21:24:27 +0300

    strings indexing made

--- src/localization/index.ts
+++ src/localization/index.ts
@@ -3,7 +3,7 @@ import { en } from './en'
 import { ru } from './ru'
 import { es } from './es'
 
-export const strings = new LocalizedStrings({
+export const strings: { [key: string]: any } = new LocalizedStrings({
     en: en,
     es: es,
     ru: ru,
