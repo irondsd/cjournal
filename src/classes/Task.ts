6361300fcc8dfc7219f65c915bf36d1f045432d0
Konstantin Mednikov <irondsd@gmail.com>
2021-03-20 15:17:43 +0300

    task redo notifiction needs testing

--- src/classes/Task.ts
+++ src/classes/Task.ts
@@ -1,4 +1,11 @@
 import timestamp from '../helpers/timestamp'
+import {
+    cancelLocalNotification,
+    scheduleNotification,
+} from '../notifications/notifications'
+import { delayNotificationBy } from '../constants'
+import { strings } from '../localization'
+import { taskCancelNotification } from '../redux/actions'
 
 export interface ITask {
     _id: string
@@ -7,10 +14,11 @@ export interface ITask {
     completed: boolean
     last_updated: number
     data: any
-    notification?: any
+    notification?: INotification
 }
 
 export interface INotification {
+    id: number
     time: number
     delayed?: number
 }
@@ -21,6 +29,9 @@ export interface ITaskClass extends ITask {
     uncomplete: () => void
     isCompleted: () => boolean
     isNewerThan: (other: ITask) => boolean
+    setNotification: (time?: number) => void
+    delayNotification: () => void
+    cancelNotification: () => void
 }
 
 export default class Task implements ITaskClass {
@@ -32,22 +43,14 @@ export default class Task implements ITaskClass {
     data: any
     notification?: any
 
-    constructor(
-        _id: string,
-        activity_type: string,
-        time: number,
-        completed: boolean,
-        last_updated: number,
-        data: any,
-        notification?: any,
-    ) {
-        this._id = _id
-        this.activity_type = activity_type
-        this.time = time
-        this.completed = completed
-        this.last_updated = last_updated
-        this.data = data
-        this.notification = notification
+    constructor(task: ITask) {
+        this._id = task._id
+        this.activity_type = task.activity_type
+        this.time = task.time
+        this.completed = task.completed
+        this.last_updated = task.last_updated
+        this.data = task.data
+        this.notification = task.notification
     }
 
     isEqual(other: ITask): boolean {
@@ -65,6 +68,35 @@ export default class Task implements ITaskClass {
     uncomplete() {
         this.completed = false
         this.last_updated = timestamp()
+    }
+
+    setNotification(time: number | undefined) {
+        scheduleNotification(
+            this.activity_type,
+            strings.notifText,
+            time || this.time,
+        )
+        this.notification = {
+            time: time || this.time,
+            delayed: 0,
+        }
+    }
+
+    delayNotification() {
+        const time = this.notification.time + delayNotificationBy
+        taskCancelNotification(this.notification.id)
+        scheduleNotification(this.activity_type, strings.notifText, time)
+        // schedule
+        this.notification = {
+            time: time,
+            delayed: this.notification.delayed + 1,
+        }
+    }
+
+    cancelNotification() {
+        taskCancelNotification(this)
+        cancelLocalNotification(this.notification.id)
+        delete this.notification
     }
 
     isCompleted(): boolean {
