3c3c745f8f747ba4dedc880156be73b550d49f07
Konstantin Mednikov <irondsd@gmail.com>
2021-03-24 23:52:34 +0300

    gps helper to ts

--- src/helpers/GPS.ts
+++ src/helpers/GPS.ts
@@ -26,7 +26,13 @@
 //:::                                                                         :::
 //:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
-export function calculateDistance(lat1, lon1, lat2, lon2, unit = 'K') {
+export function calculateDistance(
+    lat1: number,
+    lon1: number,
+    lat2: number,
+    lon2: number,
+    unit: 'K' | 'N' = 'K',
+): number {
     if (lat1 == lat2 && lon1 == lon2) {
         return 0
     } else {
@@ -34,7 +40,9 @@ export function calculateDistance(lat1, lon1, lat2, lon2, unit = 'K') {
         var radlat2 = (Math.PI * lat2) / 180
         var theta = lon1 - lon2
         var radtheta = (Math.PI * theta) / 180
-        var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta)
+        var dist =
+            Math.sin(radlat1) * Math.sin(radlat2) +
+            Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta)
         if (dist > 1) {
             dist = 1
         }
