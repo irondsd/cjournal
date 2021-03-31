d7eb6bb44975664a20a6b1ac2170aa32d319570b
Konstantin Mednikov <irondsd@gmail.com>
2021-03-31 10:11:34 +0300

    auth nav change

--- src/navigation/Auth.ts
+++ src/navigation/Auth.ts
@@ -1,6 +1,6 @@
 import { createStackNavigator } from 'react-navigation-stack'
-import WelcomeScreen from '../screens/WelcomeScreen'
-import RegisterScreen from '../screens/RegisterScreen'
+import { WelcomeScreen } from '../screens/WelcomeScreen'
+import { RegisterScreen } from '../screens/RegisterScreen'
 
 export const Auth = createStackNavigator(
     {
