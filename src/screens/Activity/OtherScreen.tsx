e0b03c3f5b9b29454d1b1c1829a78ac2de8a9bd4
Konstantin Mednikov <irondsd@gmail.com>
2021-06-24 09:49:09 +0300

    others screen done ts

--- src/screens/Activity/OtherScreen.tsx
+++ src/screens/Activity/OtherScreen.tsx
@@ -1,22 +1,18 @@
 import React, { useState, useEffect } from 'react'
-import { Platform, StyleSheet, Text, View } from 'react-native'
-import { paths, defaultStyles, prescriptions } from '../../constants'
+import { View } from 'react-native'
+import { defaultStyles, paths } from '../../constants'
 import { NavigationStackScreenComponent } from 'react-navigation-stack'
 import { strings } from '../../localization'
 import timestamp from '../../helpers/timestamp'
-import { useSelector, useDispatch } from 'react-redux'
-import { addActivity } from '../../redux/actions'
-import { findLatestTask } from '../../classes/Task'
-import { TimePicker } from '../../components/TimePicker2'
-import TakePhoto from '../../components/TakePhoto'
-import { DropDownInput } from '../../components/DropDownInput2'
+import { useDispatch } from 'react-redux'
+import { OthersPickers } from '../../components/OthersPickers'
 import Activity from '../../classes/Activity'
-import { RootState } from '../../redux/store'
 import { IAData } from '../../classes/Activity'
 import { Button } from '../../components/Button'
-import { TimeSwitch, TimeSwitchValues } from '../../components/TimeSwitchTS'
-import { DurationPicker } from '../../components/DurationPickerTS'
+import { AudioRecorder } from '../../components/AudioRecorderTS'
 import { TimePickCombined } from '../../components/TimePickCombined'
+import { addHint } from '../../services/hints'
+import { addActivity } from '../../redux/actions'
 
 type OtherActivityType = {
     activity_type?: string
@@ -24,41 +20,25 @@ type OtherActivityType = {
     time_ended?: number
 }
 
-const OtherScreen: NavigationStackScreenComponent = ({ navigation }) => {
-    const user = useSelector((state: RootState) => state.user)
+export const OtherScreen: NavigationStackScreenComponent = ({ navigation }) => {
     const dispatch = useDispatch()
     const params = navigation?.state?.params
     const [activity, setActivity] = useState<OtherActivityType>({})
     const [data, setData] = useState<IAData>({})
-    const [from, setFrom] = useState<TimeSwitchValues>(
-        TimeSwitchValues.fromStart,
-    )
-    const [duration, setDuration] = useState(0)
-
-    const updateActivityValue = (key: string, value: any) => {
-        setActivity({
-            ...activity,
-            [key]: value,
-        })
-    }
-    const updateDataValue = (key: string, value: any) => {
-        setData({
-            ...data,
-            [key]: value,
-        })
-    }
 
     const submit = () => {
-        // const newAct = Activity.init(
-        //     activity.activity_type!,
-        //     activity.time_started!,
-        //     undefined,
-        //     undefined,
-        //     undefined,
-        //     data,
-        // )
-        // dispatch(addActivity(newAct))
-        // navigation.navigate(paths.Home)
+        const newAct = Activity.init(
+            activity.activity_type,
+            activity.time_started,
+            activity.time_ended,
+            undefined,
+            undefined,
+            data,
+        )
+        // console.log(newAct)
+        if (data.type) addHint(activity.activity_type, data.type)
+        dispatch(addActivity(newAct))
+        navigation.navigate(paths.Home)
     }
 
     useEffect(() => {
@@ -85,28 +65,20 @@ const OtherScreen: NavigationStackScreenComponent = ({ navigation }) => {
             <TimePickCombined
                 time_started={activity.time_started}
                 time_ended={activity.time_ended}
-                onChange={(v, t) => console.log(v, t)}
+                onChange={(s, e) => {
+                    setActivity({ ...activity, time_started: s, time_ended: e })
+                }}
             />
-            {/* <View
-                style={
-                    Platform.OS === 'ios'
-                        ? { zIndex: 10, width: '100%' }
-                        : { width: '100%' }
-                }>
-                <DropDownInput
-                    list={this.state.list}
-                    open={true}
-                    onChangeText={text => {
-                        this.setState({
-                            type: text,
-                        })
-                    }}
-                />
-            </View>
+            <OthersPickers
+                activity_type={activity.activity_type}
+                open={true}
+                value={data.type}
+                onChange={value => setData({ ...data, type: value })}
+            />
             <AudioRecorder
-                audioFile={this.state.audioFile}
-                setAudio={this.setAudio}
-            /> */}
+                file={data.audioFile}
+                onChange={value => setData({ ...data, audioFile: value })}
+            />
             <Button title={strings.Save} onPress={submit} />
         </View>
     )
@@ -117,5 +89,3 @@ OtherScreen.navigationOptions = ({ navigation }) => {
         title: navigation.getParam('headerTitle'),
     }
 }
-
-export default OtherScreen
