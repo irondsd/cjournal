7f91abdb90b80b2574e38615ed49cd6d5822f73f
Konstantin Mednikov <irondsd@gmail.com>
2021-07-09 09:35:58 +0300

    current walking screen test

---
+++ src/screens/Activity/WalkingTestScreen.tsx
@@ -0,0 +1,196 @@
+import React, { useState, useEffect } from 'react'
+import {
+    View,
+    Text,
+    Platform,
+    StyleSheet,
+    Vibration,
+    BackHandler,
+} from 'react-native'
+import {
+    activityTypes,
+    defaultStyles,
+    paths,
+    walkingDuration,
+} from '../../constants'
+import { NavigationStackScreenComponent } from 'react-navigation-stack'
+import { strings } from '../../localization'
+import timestamp from '../../helpers/timestamp'
+import { useDispatch, useSelector } from 'react-redux'
+import Activity from '../../classes/Activity'
+import { Button } from '../../components/Button'
+import { addActivity } from '../../redux/actions'
+import { findLatestTask } from '../../classes/Task'
+import { RootState } from '../../redux/store'
+import { InfoBox } from '../../components/exercise/InfoBox'
+import { CircleProgress } from '../../components/exercise/CircleProgress'
+import { width } from '../../constants'
+import BackgroundTimer from 'react-native-background-timer'
+import { secs2time } from '../../helpers/dateTime'
+import { useGeolocation } from '../../hooks/useGeolocation'
+import { usePedometer } from '../../hooks/usePedometer'
+import { terminateAlarm } from '../../helpers/terminateAlarm'
+import { BackButton } from '../../components/BackButton'
+
+type ActivityType = {
+    activity_type?: string
+    time_started?: number
+    time_ended?: number
+    task?: string
+    comment?: string
+}
+
+const TYPE = activityTypes.WalkingTest
+
+export const WalkingTestScreen: NavigationStackScreenComponent = ({
+    navigation,
+}) => {
+    const dispatch = useDispatch()
+    const tasks = useSelector((state: RootState) => state.tasks)
+    const [activity, setActivity] = useState<ActivityType>({
+        activity_type: TYPE,
+    })
+    const { params } = navigation.state
+    const [progress, setProgress] = useState(false)
+    const [seconds, setSeconds] = useState(walkingDuration)
+    const {
+        geolocationData,
+        startUpdates: startGPS,
+        stopUpdates: stopGPS,
+    } = useGeolocation()
+
+    const {
+        pedometerData,
+        startUpdates: startPedometer,
+        stopUpdates: stopPedometer,
+    } = usePedometer()
+
+    const buttonPress = () => {
+        if (!progress) setProgress(true)
+        else backPressed()
+    }
+
+    const backPressed = () => {
+        if (progress) {
+            terminateAlarm(strings.CantGoBack, submit)
+            return true
+        }
+    }
+
+    const submit = () => {
+        setProgress(false)
+        Vibration.vibrate(600)
+
+        const newAct = Activity.init(
+            activity.activity_type,
+            activity.time_started,
+            activity.time_ended,
+            activity.task,
+            undefined,
+            {
+                steps: pedometerData.numberOfSteps,
+                distance: parseInt(pedometerData.distance.toFixed(0)),
+                locations: geolocationData,
+            },
+        )
+        // console.log(newAct)
+        dispatch(addActivity(newAct))
+        navigation.navigate(paths.Home)
+    }
+
+    useEffect(() => {
+        const task = params?.task || findLatestTask(tasks, TYPE)
+        setActivity({ ...activity, task })
+        navigation.setParams({ headerTitle: strings[TYPE] })
+
+        // prevent going back without saving
+        BackHandler.addEventListener('hardwareBackPress', backPressed)
+
+        return () =>
+            BackHandler.removeEventListener('hardwareBackPress', backPressed)
+    }, [])
+
+    useEffect(() => {
+        let timeoutId
+        let intervalId
+        let time_started
+
+        if (progress) {
+            time_started = timestamp()
+            setActivity({ ...activity, time_started })
+
+            intervalId = BackgroundTimer.setInterval(() => {
+                setSeconds(time_started - timestamp() + walkingDuration)
+            }, 200)
+
+            // timeout for 6 minutes
+            timeoutId = BackgroundTimer.setTimeout(() => {
+                submit()
+            }, walkingDuration * 1000)
+
+            startGPS()
+            startPedometer()
+        } else {
+            BackgroundTimer.clearInterval(intervalId)
+            BackgroundTimer.clearTimeout(timeoutId)
+            stopGPS()
+            stopPedometer()
+        }
+
+        return () => {
+            BackgroundTimer.clearInterval(intervalId)
+            BackgroundTimer.clearTimeout(timeoutId)
+            stopGPS()
+            stopPedometer()
+        }
+    }, [progress])
+
+    useEffect(() => {
+        if (seconds === 0) {
+            submit()
+        }
+    }, [seconds])
+    console.log(activity)
+    return (
+        <View style={defaultStyles.container}>
+            <InfoBox
+                info={{
+                    [strings.Steps]: pedometerData.numberOfSteps,
+                    [strings.Distance]: `${pedometerData.distance.toFixed(0)} ${
+                        strings.m
+                    }`,
+                }}
+            />
+            <CircleProgress progress={seconds / 3.6}>
+                <Text style={styles.timer}>
+                    {activity.time_started
+                        ? secs2time(seconds)
+                        : secs2time(walkingDuration)}
+                </Text>
+            </CircleProgress>
+            <Button
+                title={progress ? strings.Terminate : strings.Start}
+                onPress={() => {
+                    buttonPress()
+                }}
+            />
+        </View>
+    )
+}
+
+WalkingTestScreen.navigationOptions = ({ navigation }) => {
+    return {
+        title: navigation.getParam('headerTitle'),
+        headerLeft: null,
+    }
+}
+
+const styles = StyleSheet.create({
+    timer: {
+        fontSize: width / 7,
+        fontWeight: '200',
+        color: '#00000055',
+        transform: [{ scaleX: -1 }],
+        fontFamily: Platform.OS === 'android' ? 'sans-serif-light' : undefined,
+    },
+})
---
+++ src/screens/Activity/WalkingTestScreenOld.js
@@ -0,0 +1,310 @@
+import React, { Component } from 'react'
+import {
+    Platform,
+    StyleSheet,
+    Text,
+    View,
+    Alert,
+    BackHandler,
+    Vibration,
+} from 'react-native'
+import { secs2time, getUtcOffset } from '../../helpers/dateTime'
+import BackgroundTimer from 'react-native-background-timer'
+import { strings } from '../../localization'
+import { addActivity } from '../../redux/actions'
+import { connect } from 'react-redux'
+import { HeaderBackButton } from 'react-navigation'
+import { calculateDistance } from '../../helpers/GPS'
+import {
+    backgroundColor,
+    activityTypes,
+    paths,
+    defaultStyles,
+    walkingDuration,
+    appColor,
+    secondaryColor,
+    secondaryGrey,
+    width,
+} from '../../constants'
+import { BackButton } from '../../components/BackButton'
+import { showError } from '../../services/toast'
+import Activity from '../../classes/Activity'
+import timestamp from '../../helpers/timestamp'
+import requestLocationPermissions from '../../permissions/requestLocationPermissions'
+import GPS from '../../sensors/GPS'
+import Pedometer from '../../sensors/Pedometer'
+import SaveButton from '../../components/SaveButton'
+import { findLatestTask } from '../../classes/Task'
+import { AnimatedCircularProgress } from 'react-native-circular-progress'
+import { createIconSetFromFontello } from 'react-native-vector-icons'
+
+let timerOn = false
+
+class WalkingTestScreen extends Component {
+    static navigationOptions = ({ navigation }) => ({
+        title: strings.WalkingTest,
+        headerLeft: (
+            <BackButton
+                onPress={() => {
+                    if (timerOn) Alert.alert(strings.Alert, strings.CantGoBack)
+                    else navigation.goBack()
+                }}
+            />
+        ),
+    })
+
+    constructor(props) {
+        super(props)
+
+        this.handleBackButton = this.handleBackButton.bind(this)
+
+        this.state = {
+            timer: '0:00',
+            task: null,
+            progress: 100,
+            distance: 0,
+            steps: 0,
+            started: false,
+            timestampStart: 0,
+            timestampEnd: 0,
+        }
+
+        this.GPS = new GPS()
+        this.Pedometer = new Pedometer()
+    }
+
+    handleBackButton() {
+        if (this.state.timer_set) {
+            return true
+        }
+    }
+
+    componentDidMount() {
+        requestLocationPermissions()
+        BackHandler.addEventListener('hardwareBackPress', this.handleBackButton)
+        let task = findLatestTask(this.props.tasks, activityTypes.WalkingTest)
+        if (
+            this.props.navigation.state.params &&
+            this.props.navigation.state.params.task
+        ) {
+            task = this.props.navigation.state.params.task
+        }
+        this.setState({
+            timer: secs2time(walkingDuration),
+            task: task,
+        })
+    }
+
+    componentWillUnmount() {
+        BackHandler.removeEventListener(
+            'hardwareBackPress',
+            this.handleBackButton,
+        )
+
+        this.Pedometer.stopUpdates()
+        this.GPS.watchStop()
+        this.timerStop()
+        timerOn = false
+    }
+
+    record() {
+        let data = {
+            steps: this.state.steps,
+            distance: this.state.distance,
+            position: this.GPS.getFirstAndLastPosition(),
+        }
+
+        let activity = Activity.init(
+            activityTypes.WalkingTest,
+            this.state.timestampStart,
+            timestamp(),
+            this.state.task,
+            undefined,
+            data,
+        )
+        this.props.add(activity)
+        this.props.navigation.navigate(paths.ExerciseFinish, {
+            activity: activity,
+        })
+    }
+
+    timerStart() {
+        timerOn = true
+        // this timer is to update the screen
+        this.intervalId = BackgroundTimer.setInterval(() => {
+            this.timerTick()
+        }, 1000)
+
+        // timeout for 6 minutes
+        this.timeoutId = BackgroundTimer.setTimeout(() => {
+            this.timerStop()
+            this.record()
+        }, walkingDuration * 1000)
+
+        this.setState(prevstate => ({
+            started: true,
+            timestampStart: timestamp(),
+            timestampEnd: timestamp() + walkingDuration,
+        }))
+
+        this.GPS.watchStart()
+        this.Pedometer.startUpdates(new Date())
+    }
+
+    startPressed() {
+        if (!this.state.started) {
+            this.timerStart()
+        } else {
+            Alert.alert(strings.Cancel, strings.TerminationExerciseQuestion, [
+                {
+                    text: strings.Cancel,
+                    style: 'cancel',
+                },
+                {
+                    text: strings.Terminate,
+                    onPress: () => {
+                        this.timerStop()
+                        this.record()
+                    },
+                },
+            ])
+        }
+    }
+
+    timerTick() {
+        let time = this.state.timestampEnd - timestamp()
+
+        // console.log(time)
+
+        this.setState({
+            timer: secs2time(time),
+            distance: this.GPS.distance,
+            steps: this.Pedometer.steps,
+            progress: time / 3.6,
+        })
+
+        if (time === 0) {
+            // time ran out
+            Vibration.vibrate(600)
+            this.timerStop()
+            this.record()
+        }
+        if (time < 0) {
+            // time ran out before
+            this.setState({
+                timer: '00:00',
+            })
+            this.timerStop()
+            this.record()
+        }
+    }
+
+    timerStop() {
+        BackgroundTimer.clearInterval(this.intervalId)
+        BackgroundTimer.clearTimeout(this.timeoutId)
+        this.GPS.watchStop()
+        this.Pedometer.stopUpdates()
+
+        timerOn = false
+    }
+
+    render() {
+        return (
+            <View style={defaultStyles.container}>
+                <View style={styles.stats}>
+                    <View style={styles.textSteps}>
+                        <Text style={styles.text}>{strings.Steps}</Text>
+                        <Text style={styles.number}>{this.state.steps}</Text>
+                    </View>
+                    <View style={styles.textDistance}>
+                        <Text style={styles.text}>{strings.Distance}</Text>
+                        <Text style={styles.number}>{this.state.distance}</Text>
+                    </View>
+                </View>
+                <View style={styles.timerView}>
+                    <AnimatedCircularProgress
+                        size={width / 1.3}
+                        width={2}
+                        fill={this.state.progress}
+                        rotation={180}
+                        tintColor={'#00000055'}
+                        backgroundColor="#00000000">
+                        {() => (
+                            <Text style={styles.timer}>{this.state.timer}</Text>
+                        )}
+                    </AnimatedCircularProgress>
+                </View>
+                <View style={styles.button}>
+                    <SaveButton
+                        title={
+                            this.state.started
+                                ? strings.Terminate
+                                : strings.Start
+                        }
+                        onPress={() => {
+                            this.startPressed()
+                        }}
+                    />
+                </View>
+            </View>
+        )
+    }
+}
+
+function mapStateToProps(state) {
+    return {
+        idinv: state.user.idinv,
+        tasks: state.tasks,
+    }
+}
+
+const mapDispatchToProps = dispatch => ({
+    add: activity => {
+        dispatch(addActivity(activity))
+    },
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(WalkingTestScreen)
+
+const styles = StyleSheet.create({
+    stats: {
+        width: '100%',
+        flexDirection: 'row',
+        height: 50,
+    },
+    textSteps: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRightWidth: 1,
+        borderColor: '#dddddd',
+    },
+    textDistance: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderLeftWidth: 1,
+        borderColor: '#dddddd',
+    },
+    text: {
+        fontSize: 15,
+    },
+    number: {
+        fontSize: 25,
+    },
+    timerView: {
+        flex: 3,
+        justifyContent: 'center',
+    },
+    timer: {
+        fontSize: width / 5,
+        fontWeight: '200',
+        color: '#00000055',
+        fontFamily: Platform.OS === 'android' ? 'sans-serif-light' : undefined,
+    },
+    button: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        width: '100%',
+    },
+})
